b6eaccaa61d9db5a27beefc5aabd786ff99776fc
gho-ramos <guilherme.hor@gmail.com>
2017-11-27 23:40:21 -0200

    New features for this version
    - New listing methods: Listing (TableView) and Grid (CollectionView);
    - New Meme Details page;
    - New assets;

---
+++ MeMe/List Memes/GridCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  GridCollectionViewCell.swift
+//  MeMe
+//
+//  Created by Guilherme Ramos on 22/11/2017.
+//  Copyright © 2017 Guilherme Ramos. All rights reserved.
+//
+
+import UIKit
+
+class GridCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+}
