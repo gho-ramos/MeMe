dd8e73a928d88c5729dcf1d7133b758f8e359004
gho-ramos <guilherme.hor@gmail.com>
2017-11-30 01:33:45 -0200

    New editting meme feature
    - The user can now check details from memes and edit them;

--- MeMe/List Memes/MemeDetailViewController.swift
+++ MeMe/List Memes/MemeDetailViewController.swift
@@ -8,17 +8,35 @@
 
 import UIKit
 
-class MemeDetailViewController: BaseViewController {
+protocol MemeDetailViewControllerDelegate: NSObjectProtocol {
+    func didFinishEditing(meme: Meme?)
+}
 
-    var memedImage: UIImage?
+class MemeDetailViewController: BaseViewController {
+    var index: Int?
+
+    fileprivate var meme: Meme?
+
     @IBOutlet weak var imageView: UIImageView!
+
+    weak var applicationDelegate: AppDelegate?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        applicationDelegate = UIApplication.shared.delegate as? AppDelegate
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            barButtonSystemItem: .edit,
+            target: self,
+            action: #selector(editAction(_: ))
+        )
     }
 
     override func viewWillAppear(_ animated: Bool) {
-        imageView.image = memedImage
+
+        if let index = index {
+            meme = applicationDelegate?.memes[index]
+            imageView.image = meme?.memedImage
+        }
     }
 
     class func instance() -> Self {
@@ -28,10 +46,33 @@ class MemeDetailViewController: BaseViewController {
         )
     }
 
-    class func instance(with image: UIImage?) -> Self {
+    class func instance(with index: Int?) -> Self {
         let controller = instance()
-        controller.memedImage = image
+        controller.index = index
         return controller
     }
 
+    @IBAction func editAction(_ sender: AnyObject?) {
+        guard let meme = meme else {
+            return
+        }
+        let controller = CreateNewMemeViewController.instance()
+        controller.meme = meme
+        controller.delegate = self
+        present(controller, animated: true, completion: nil)
+    }
+
+}
+
+extension MemeDetailViewController: MemeDetailViewControllerDelegate {
+    // Using AnyObject because couldn't find another way to use a struct as a parameter for this delegate
+    func didFinishEditing(meme: Meme?) {
+        self.meme = meme
+
+        guard let applicationDelegate = applicationDelegate else {
+            return
+        }
+
+        applicationDelegate.memes.remove(at: index!)
+    }
 }
